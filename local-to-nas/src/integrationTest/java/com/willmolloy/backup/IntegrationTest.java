40bbd7feb6877a89f9b07455cb7427f323fc042a
Will Molloy <willjoemolloy@gmail.com>
2022-04-29 11:11:50 +1200

    More test scenarios

--- local-to-nas/src/integrationTest/java/com/willmolloy/backup/IntegrationTest.java
+++ local-to-nas/src/integrationTest/java/com/willmolloy/backup/IntegrationTest.java
@@ -4,6 +4,7 @@ import static com.google.common.base.Preconditions.checkNotNull;
 import static com.google.common.truth.Truth.assertThat;
 
 import com.github.javafaker.Faker;
+import com.google.common.collect.Iterables;
 import com.google.common.truth.Correspondence;
 import com.willmolloy.backup.util.DirectoryWalker;
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
@@ -49,6 +50,18 @@ class IntegrationTest {
   }
 
   @Test
+  void given_fileOnlyOnSource_then_copiesFileToDestination() throws IOException {
+    // Given
+    Path sourceFile1 = createFileAt(source.resolve("file1"));
+
+    // When
+    Main.main(source.toString(), destination.toString(), "false");
+
+    // Then
+    assertThatSourceAndDestinationContainsExactlyRelativeFromSource(sourceFile1);
+  }
+
+  @Test
   void given_filesOnlyOnSource_then_copiesFilesToDestination() throws IOException {
     // Given
     Path sourceFile1 = createFileAt(source.resolve("file1"));
@@ -61,8 +74,35 @@ class IntegrationTest {
     Main.main(source.toString(), destination.toString(), "false");
 
     // Then
-    assertThatSourceAndDestinationContainExactlyRelativeFromSource(
+    assertThatSourceAndDestinationContainsExactlyRelativeFromSource(
         sourceFile1, sourceFile2, sourceNestedFile1, sourceNestedFile2, sourceEmptyDirectory);
+  }
+
+  @Test
+  void given_emptyDirectoryOnlyOnSource_then_copiesEmptyDirectoryToDestination()
+      throws IOException {
+    // Given
+    Path sourceEmptyDirectory = createDirectoryAt(source.resolve("my-directory"));
+
+    // When
+    Main.main(source.toString(), destination.toString(), "false");
+
+    // Then
+    assertThatSourceAndDestinationContainsExactlyRelativeFromSource(sourceEmptyDirectory);
+  }
+
+  @Test
+  void given_fileOnBothSourceAndDestination_then_replacesFileOnDestination() throws IOException {
+    // Given
+    Path sourceFile1 = createFileAt(source.resolve("file1"));
+
+    Path destinationFile1 = createFileAt(destination.resolve("file1"));
+
+    // When
+    Main.main(source.toString(), destination.toString(), "false");
+
+    // Then
+    assertThatSourceAndDestinationContainsExactlyRelativeFromSource(sourceFile1);
   }
 
   @Test
@@ -84,8 +124,35 @@ class IntegrationTest {
     Main.main(source.toString(), destination.toString(), "false");
 
     // Then
-    assertThatSourceAndDestinationContainExactlyRelativeFromSource(
+    assertThatSourceAndDestinationContainsExactlyRelativeFromSource(
         sourceFile1, sourceFile2, sourceNestedFile1, sourceNestedFile2, sourceEmptyDirectory);
+  }
+
+  @Test
+  void given_emptyDirectoryOnBothSourceAndDestination_then_replacesEmptyDirectoryOnDestination()
+      throws IOException {
+    // Given
+    Path sourceEmptyDirectory = createDirectoryAt(source.resolve("user/documents"));
+
+    Path destinationEmptyDirectory = createDirectoryAt(destination.resolve("user/documents"));
+
+    // When
+    Main.main(source.toString(), destination.toString(), "false");
+
+    // Then
+    assertThatSourceAndDestinationContainsExactlyRelativeFromSource(sourceEmptyDirectory);
+  }
+
+  @Test
+  void given_fileOnlyOnDestination_then_deletesFileOnDestination() throws IOException {
+    // Given
+    Path destinationFile1 = createFileAt(destination.resolve("file1"));
+
+    // When
+    Main.main(source.toString(), destination.toString(), "false");
+
+    // Then
+    assertThatSourceAndDestinationContainsExactlyRelativeFromSource();
   }
 
   @Test
@@ -101,7 +168,96 @@ class IntegrationTest {
     Main.main(source.toString(), destination.toString(), "false");
 
     // Then
-    assertThatSourceAndDestinationContainExactlyRelativeFromSource();
+    assertThatSourceAndDestinationContainsExactlyRelativeFromSource();
+  }
+
+  @Test
+  void given_emptyDirectoryOnlyOnDestination_then_deletesEmptyDirectoryOnDestination()
+      throws IOException {
+    // Given
+    Path destinationEmptyDirectory = createDirectoryAt(destination.resolve("user/documents"));
+
+    // When
+    Main.main(source.toString(), destination.toString(), "false");
+
+    // Then
+    assertThatSourceAndDestinationContainsExactlyRelativeFromSource();
+  }
+
+  @Test
+  void given_directoryOnSourceAndSubDirectoryOnDestination_then_deletesSubDirectoryOnDestination()
+      throws IOException {
+    // Given
+    Path sourceEmptyDirectory = createDirectoryAt(source.resolve("user/documents"));
+
+    Path destinationEmptyDirectory = createDirectoryAt(destination.resolve("user/documents/work"));
+
+    // When
+    Main.main(source.toString(), destination.toString(), "false");
+
+    // Then
+    assertThatSourceAndDestinationContainsExactlyRelativeFromSource(sourceEmptyDirectory);
+  }
+
+  @Test
+  void given_directoryOnSourceAndSuperDirectoryOnDestination_then_createsSubDirectoryOnDestination()
+      throws IOException {
+    // Given
+    Path sourceEmptyDirectory = createDirectoryAt(source.resolve("user/documents/work"));
+
+    Path destinationEmptyDirectory = createDirectoryAt(destination.resolve("user/documents"));
+
+    // When
+    Main.main(source.toString(), destination.toString(), "false");
+
+    // Then
+    assertThatSourceAndDestinationContainsExactlyRelativeFromSource(sourceEmptyDirectory);
+  }
+
+  @Test
+  void given_filesOnlyOnSource_and_dryRun_then_doesNothing() throws IOException {
+    // Given
+    Path sourceFile1 = createFileAt(source.resolve("file1"));
+    Path sourceFile2 = createFileAt(source.resolve("file2"));
+    Path sourceNestedFile1 = createFileAt(source.resolve("nested/file1"));
+    Path sourceNestedFile2 = createFileAt(source.resolve("nested/directory/file2"));
+    Path sourceEmptyDirectory = createDirectoryAt(source.resolve("user/documents"));
+
+    // When
+    Main.main(source.toString(), destination.toString(), "true");
+
+    // Then
+    assertThatDirectoryContainsExactly(
+        source,
+        sourceFile1,
+        sourceFile2,
+        sourceNestedFile1,
+        sourceNestedFile2,
+        sourceEmptyDirectory);
+    assertThatDirectoryContainsExactly(destination);
+  }
+
+  @Test
+  void given_filesOnlyDestination_and_dryRun_then_doesNothing() throws IOException {
+    // Given
+    Path destinationFile1 = createFileAt(destination.resolve("file1"));
+    Path destinationFile2 = createFileAt(destination.resolve("file2"));
+    Path destinationNestedFile1 = createFileAt(destination.resolve("nested/file1"));
+    Path destinationNestedFile2 = createFileAt(destination.resolve("nested/directory/file2"));
+    Path destinationEmptyDirectory = createDirectoryAt(destination.resolve("user/documents"));
+
+    // When
+    Main.main(source.toString(), destination.toString(), "true");
+
+    // Then
+    assertThatDirectoryContainsExactly(source);
+    assertThatDirectoryContainsExactly(
+        destination,
+        destinationFile1,
+        destinationFile2,
+        destinationNestedFile1,
+        destinationNestedFile2,
+        destinationEmptyDirectory);
   }
 
   private Path createFileAt(Path path) throws IOException {
@@ -119,42 +275,44 @@ class IntegrationTest {
     return path;
   }
 
-  private void assertThatSourceAndDestinationContainExactlyRelativeFromSource(
-      Path... expectedLeaves) {
-    for (Path directory : List.of(source, destination)) {
-      List<Path> expectedLeavesRelativeFromSource =
-          Arrays.stream(expectedLeaves)
-              .map(
-                  path -> {
-                    Path relativeFromSource = source.relativize(path);
-                    return directory.resolve(relativeFromSource);
-                  })
-              .toList();
-      assertThat(directoryWalker.leavesExcludingSelf(directory).toList())
-          .comparingElementsUsing(pathsEquivalent())
-          .containsExactlyElementsIn(expectedLeavesRelativeFromSource);
-    }
+  private void assertThatSourceAndDestinationContainsExactlyRelativeFromSource(
+      Path... expectedLeavesRelativeFromSource) {
+    assertThatDirectoryContainsExactly(source, expectedLeavesRelativeFromSource);
+
+    List<Path> expectedLeavesRelativeFromDestination =
+        Arrays.stream(expectedLeavesRelativeFromSource)
+            .map(
+                path -> {
+                  Path relativeFromSource = source.relativize(path);
+                  return destination.resolve(relativeFromSource);
+                })
+            .toList();
+    assertThatDirectoryContainsExactly(
+        destination, Iterables.toArray(expectedLeavesRelativeFromDestination, Path.class));
+  }
+
+  private void assertThatDirectoryContainsExactly(Path directory, Path... expectedLeaves) {
+    assertThat(directoryWalker.leavesExcludingSelf(directory).toList())
+        .comparingElementsUsing(pathsEquivalent())
+        .containsExactlyElementsIn(expectedLeaves);
   }
 
   private Correspondence<Path, Path> pathsEquivalent() {
-    Correspondence.BinaryPredicate<Path, Path> recordsEquivalent =
-        (actual, expected) -> {
-          try {
-            return actual.equals(expected) && Files.mismatch(actual, expected) == -1;
-          } catch (IOException e) {
-            throw new UncheckedIOException(e);
-          }
-        };
-
-    Correspondence.DiffFormatter<Path, Path> diffFormatter =
-        (actual, expected) -> {
-          if (!actual.equals(expected)) {
-            return "paths not equal";
-          }
-          return "contents not same";
-        };
-
-    return Correspondence.from(recordsEquivalent, "is equal to with same contents")
-        .formattingDiffsUsing(diffFormatter);
+    return Correspondence.<Path, Path>from(
+            (actual, expected) -> {
+              try {
+                return actual.equals(expected) && Files.mismatch(actual, expected) == -1;
+              } catch (IOException e) {
+                throw new UncheckedIOException(e);
+              }
+            },
+            "is equal to with same contents")
+        .formattingDiffsUsing(
+            (actual, expected) -> {
+              if (!actual.equals(expected)) {
+                return "paths not equal";
+              }
+              return "contents not same";
+            });
   }
 }
