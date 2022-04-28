8b60f8c2e5f2a2c8f855b215dbf2a6be6c6ceeeb
Will <willjoemolloy@gmail.com>
2022-04-28 20:57:07 +1200

    Initial commit

---
+++ hello-world/src/main/java/com/willmolloy/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.willmolloy;
+
+/**
+ * Example main src.
+ *
+ * @author <a href=https://willmolloy.com>Will Molloy</a>
+ */
+public class HelloWorld {
+
+  public String helloWorld() {
+    return "Hello world!";
+  }
+}
