6e57cc22659217ecf3896ec8dab66aa71325f1e0
Will Molloy <willjoemolloy@gmail.com>
2022-04-29 02:42:07 +1200

    Update type param format
    Use C# style

--- local-to-nas/src/main/java/com/willmolloy/backup/FileBackup.java
+++ local-to-nas/src/main/java/com/willmolloy/backup/FileBackup.java
@@ -4,7 +4,7 @@ package com.willmolloy.backup;
  * File backup contract.
  *
  * @param <TSource> Source type
- * @param <TDestination>> Destination type
+ * @param <TDestination> Destination type
  * @author <a href=https://willmolloy.com>Will Molloy</a>
  */
 public interface FileBackup<TSource, TDestination> {
